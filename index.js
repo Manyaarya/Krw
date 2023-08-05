3fa8ff1a70b03dce02d510caa88ac2624caf37c7
Manyaarya <manya043btcsai21@igdtuw.ac.in>
2023-08-05 14:41:11 +0530

    I started working on Backend, Basic structure of website is done using HTML and CSS.

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
-const manya = require("./secondnode");
+const slideshow = document.querySelector('.slideshow');
+const images = slideshow.querySelectorAll('img');
 
-console.log("Hello World", manya)
+let currentIndex = 0;
+
+function showImage(index) {
+    images.forEach((image, i) => {
+        image.classList.remove('active');
+        if (i === index) {
+            image.classList.add('active');
+        }
+    });
+}
+
+function nextImage() {
+    currentIndex = (currentIndex + 1) % images.length;
+    showImage(currentIndex);
+}
+
+// Start the slideshow
+setInterval(nextImage, 3000); // Change image every 3 seconds
 
